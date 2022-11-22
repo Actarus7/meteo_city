d3a62fa1999c05ad37221c7002011a938fa3315d
Steff <steff.1@gmail.com>
2022-11-22 15:11:31 +0100

    Récupération de toutes les infos mise dans un objet (nom, températures, temps, humidité etc)

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const form = document.getElementById("entrerVilleForm")
-const arrUserVille = []
 
 form.addEventListener("submit", event => {
     event.preventDefault();
@@ -7,50 +6,62 @@ form.addEventListener("submit", event => {
 
     const userVille = document.getElementById("entrerVille");
     const userVilleText = userVille.value;
-    //console.log(userVilleText); 
-    getLatLong(userVilleText)
-
+    console.log(userVilleText); 
+    getWeatherInfos(userVilleText);
 })
 
-function getLatLong (userVilleText) {
+function getWeatherInfos(userVilleText) {
     const urlAPIGouv = `https://geo.api.gouv.fr/communes?nom=${userVilleText}&fields=centre`;
     fetch(urlAPIGouv)
     .then(function(data) {
-        console.log(data);
+        //console.log(data);
         return data.json();
     })
     .then(function(data){
-        console.log(data
-            );
+        //console.log(data);
         return data;
     })
     .then(function(data){
-        console.log(data[0].nom);
         if (data[0].nom == userVilleText) {
             const userVilleInfos = {
                 name : data[0].nom,
                 lat : data[0].centre.coordinates[0],
                 long : data[0].centre.coordinates[1],
             };
-            return userVilleInfos}    
+            //console.log(userVilleInfos)
+            return userVilleInfos;
+        }    
+    })
+    .then(function(userVilleInfos){
+        const API_key = "316bfa59b3b302dc294f8e139f0d0fea";
+        const urlOpenWeatherAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${userVilleInfos.lat}&lon=${userVilleInfos.long}&appid=${API_key}&units=metric`;
+        fetch(urlOpenWeatherAPI)
+        .then(function(data){
+            return data.json();
+        })
+        .then(function(data){
+            //console.log(data);
+            return data;
+        })
+        .then(function(data){
+            const villeWeatherInfos = {
+                name : userVilleInfos.name,
+                temperature : data.main.temp,
+                temperature_min : data.main.temp_min,
+                temperature_max : data.main.temp_max,
+                temperature_ressentie : data.main.feels_like,
+                wind_speed : data.wind.speed,
+                wind_direction : data.wind.deg,
+                humidity : data.main.humidity,
+                weather : data.weather[0].icon,
+            };
+            //console.log(villeWeatherInfos);
+            resultTest(villeWeatherInfos);
+            return villeWeatherInfos;
+        })
     })
 }
-
-/* function getUserVille() {
-    
+function resultTest(villeWeatherInfos){
+    console.log(villeWeatherInfos);
     
 }
-const urlAPIGouv = `https://geo.api.gouv.fr/communes?nom=${userVille}&fields=code,nom,centre`
-
-
-
-
-const urlAPIMeteo = `https://openweathermap.org/`
-const key = "316bfa59b3b302dc294f8e139f0d0fea"
-fetch(url)
-.then(function() {
-
-})
-.catch(function() {
-
-}); */
