6a55d82cf611be94190bbb30ce67da7a9c767918
Steff <steff.1@gmail.com>
2022-11-24 12:54:14 +0100

    Retouches finales

--- script.js
+++ script.js
@@ -13,38 +13,21 @@ buttonClick.addEventListener("click", event => {
 })
 
 
-function getWeatherInfos(userVilleText) {
-    const urlAPIGouv = `https://geo.api.gouv.fr/communes?nom=${userVilleText}&fields=centre`;
-    fetch(urlAPIGouv)
-    .then(function(promise) {
-        return promise.json();
-    })
-    .then(function(data){
-        return data;
-    })
-    .then(function(data){
-        if (data[0].nom == userVilleText) {
-            const userVilleInfos = {
-                name : data[0].nom,
-                lat : data[0].centre.coordinates[1],
-                long : data[0].centre.coordinates[0],
-            };
-            return userVilleInfos;
-        }    
-    })
-    .then(function(userVilleInfos){
+function getWeatherInfos(ville) {
         const API_key = "316bfa59b3b302dc294f8e139f0d0fea";
-        const urlOpenWeatherAPI = `https://api.openweathermap.org/data/2.5/weather?lat=${userVilleInfos.lat}&lon=${userVilleInfos.long}&appid=${API_key}&units=metric`;
+        const urlOpenWeatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=${API_key}&units=metric`;
         fetch(urlOpenWeatherAPI)
         .then(function(promise){
+            console.log(promise);
             return promise.json();
         })
         .then(function(data){
+            console.log(data);
             return data;
         })
         .then(function(data){
             const villeWeatherInfos = {
-                name : userVilleInfos.name,
+                name : data.name,
                 temperature : data.main.temp,
                 temperature_min : data.main.temp_min,
                 temperature_max : data.main.temp_max,
@@ -57,8 +40,8 @@ function getWeatherInfos(userVilleText) {
             affichage(villeWeatherInfos);
             return villeWeatherInfos;
         })
-    })
-}
+    }
+
 
 function affichage(villeWeatherInfos) {
     document.getElementsByTagName("h2")[0].textContent = villeWeatherInfos.name;
